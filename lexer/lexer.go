691490df22dbdff356c544b4ca038422c1ff3150
majkelbed <bednarczyk.business@gmail.com>
2024-01-25 21:16:21 +0100

    init

---
+++ lexer/lexer.go
@@ -0,0 +1,92 @@
+// lexer/lexer.go
+package lexer
+
+import "app/token"
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var t token.Token
+
+	//
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		t = newToken(token.ASSIGN, l.ch)
+	case ';':
+		t = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		t = newToken(token.LPAREN, l.ch)
+	case ')':
+		t = newToken(token.RPAREN, l.ch)
+	case ',':
+		t = newToken(token.COMMA, l.ch)
+	case '+':
+		t = newToken(token.PLUS, l.ch)
+	case '{':
+		t = newToken(token.LBRACE, l.ch)
+	case '}':
+		t = newToken(token.RBRACE, l.ch)
+	case 0:
+		t.Literal = ""
+		t.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			t.Literal = l.readIdentifier()
+			t.Type = token.LookupIdent(t.Literal)
+			return t
+		} else {
+			t = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+	// [...]
+	l.readChar()
+
+	return t
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
